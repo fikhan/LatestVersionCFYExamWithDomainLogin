50c5b0120358c01559628ba5b7a95b1c833a89ab
Faraz Idris Khan <fikhan00@gmail.com>
2021-01-25 15:03:06 +0300

    latest

---
+++ Controllers/Shared/UserGroupController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CFY_Exam.Controllers.Shared
+{
+    public class UserGroupController : Controller
+    {
+        public IActionResult GroupIndex()
+        {
+            return View();
+        }
+    }
+}
